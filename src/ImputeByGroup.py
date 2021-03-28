5242644e2cbb94023e570327d84c4b5a8475ccfd
Everest Law <everestl@usc.edu>
2021-03-28 02:32:38 -0700

    groupby_col now an optional argument

--- src/ImputeByGroup.py
+++ src/ImputeByGroup.py
@@ -10,7 +10,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 class ImputeByGroups:
     """ Collect common attributes used by group-by-imputers """
     target_col: str  # the ONE column to be imputed 
-    groupby_col: Iterable[str]  # the columns on which to pd.DataFrame.groupby
+    groupby_col: Iterable[str] = None  # the columns on which to pd.DataFrame.groupby, if any
     imputation_values: Any = None  # dict(-like) obj mapping from grouped by
     # return_df: bool = True  # whether to return a DataFrame or just the imputed array
     copy: bool = True  # return a copy, or modify in-place? Only relevant when return_df == True
@@ -22,13 +22,16 @@ class ImputeByGroups:
 
 class ImputeNumericalByGroups(ImputeByGroups, BaseEstimator, TransformerMixin):
     """ Impute ONE continuous numerical column, optionally after grouping by other (discrete) columns """
-    def __init__(self, target_col, groupby_col, **kwargs):
+    def __init__(self, target_col, groupby_col=None, **kwargs):
         super().__init__(target_col=target_col, groupby_col=groupby_col, **kwargs)
 
     def fit(self, X: pd.DataFrame, y=None):
         """ Implement the imputation logic here """
         assert isinstance(X, pd.DataFrame)
-        self.imputation_values = X.groupby(self.groupby_col)[self.target_col].median().to_dict()
+        if self.groupby_col:
+            self.imputation_values = X.groupby(self.groupby_col)[self.target_col].median().to_dict()
+        else:
+            self.imputation_values = X[self.target_col].median()
         return self
     
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
@@ -36,10 +39,13 @@ class ImputeNumericalByGroups(ImputeByGroups, BaseEstimator, TransformerMixin):
         imputed_df = X.copy() if self.copy else X
 
         na_mask = imputed_df[self.target_col].isna()
-        imputed_df.loc[na_mask, self.target_col] = imputed_df.loc[na_mask].apply(
-            lambda row: self.imputation_values[ tuple(row.loc[self.groupby_col]) ],
-            axis=1
-        )
+        if self.groupby_col:
+            imputed_df.loc[na_mask, self.target_col] = imputed_df.loc[na_mask].apply(
+                lambda row: self.imputation_values[ tuple(row.loc[self.groupby_col]) ],
+                axis=1
+            )
+        else:
+            imputed_df.loc[:, self.target_col].fillna(self.imputation_values, inplace=True)
     
         return imputed_df
         
