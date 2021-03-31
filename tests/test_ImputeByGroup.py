5eb033545060c9e942e2a031d4a5eb538a0ad9a9
Everest Law <everestl@usc.edu>
2021-03-31 00:22:33 -0700

    Test cases for 0,1,2 groupby columns

---
+++ tests/test_ImputeByGroup.py
@@ -0,0 +1,73 @@
+""" 
+Run tests from parent directory, which
+    - is one level up from tests/;
+    - contains src/
+"""
+
+import numpy as np
+import pandas as pd
+import pytest
+from src.ImputeByGroup import ImputeNumericalByGroups
+
+
+@pytest.fixture
+def titanic_data():
+    titanic_train = pd.read_csv("tests/titanic_train.csv")
+    titanic_test = pd.read_csv("tests/titanic_test.csv")
+    return titanic_train, titanic_test
+
+
+def test_numerical_df_wide(titanic_data):
+    """ no grouping by; just impute using dataframe-wide median """
+    titanic_train, _ = titanic_data
+    na_mask = titanic_train["Age"].isna()
+
+    imputer = ImputeNumericalByGroups(target_col="Age", copy=True, return_df=True).fit(
+        titanic_train
+    )
+    df = imputer.transform(titanic_train)
+    assert df["Age"].isna().sum() == 0
+    assert id(df) != id(titanic_train)
+    assert np.all(df.loc[na_mask, "Age"] == 28.0)
+
+
+def test_numerical_gb1(titanic_data):
+    """ gb1 stands for grouping by 1 columns """
+    titanic_train, _ = titanic_data
+    na_mask = titanic_train["Age"].isna()
+
+    imputer = ImputeNumericalByGroups(
+        target_col="Age", groupby_col=["Pclass"], copy=True, return_df=True
+    ).fit(titanic_train)
+    df = imputer.transform(titanic_train)
+    assert df["Age"].isna().sum() == 0
+    assert id(df) != id(titanic_train)
+    assert np.all(
+        df.loc[na_mask].apply(
+            lambda row: row["Age"] == imputer.imputation_values[(row["Pclass"],)],
+            axis=1,
+        )
+    )
+
+
+def test_numerical_gb2(titanic_data):
+    """ gb2 stands for grouping by 2 columns """
+    titanic_train, _ = titanic_data
+    na_mask = titanic_train["Age"].isna()
+
+    imputer = ImputeNumericalByGroups(
+        target_col="Age", groupby_col=["Pclass", "Embarked"], copy=True, return_df=True
+    ).fit(titanic_train)
+    df = imputer.transform(titanic_train)
+    assert df["Age"].isna().sum() == 0
+    assert id(df) != id(titanic_train)
+    assert np.all(
+        df.loc[na_mask].apply(
+            lambda row: row["Age"] == imputer.imputation_values[tuple(row[imputer.groupby_col])],
+            axis=1,
+        )
+    )
+
+
+# def test_numerical_without_copy(titanic_data):
+#     titanic_train, titanic_test = titanic_data
