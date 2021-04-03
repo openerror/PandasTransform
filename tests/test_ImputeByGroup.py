6e1ac4dc8361594b886c28f455e0ec66e9beed27
Everest Law <everestl@usc.edu>
2021-04-02 17:10:04 -0700

    Collected tests for the same imputer into one function

--- tests/test_ImputeByGroup.py
+++ tests/test_ImputeByGroup.py
@@ -17,62 +17,12 @@ def titanic_data():
     return titanic_train, titanic_test
 
 
-# def test_numerical_df_wide(titanic_data):
-#     """ no grouping by; just impute using dataframe-wide median """
-#     titanic_train, _ = titanic_data
-#     na_mask = titanic_train["Age"].isna()
-
-#     imputer = ImputeNumericalByGroup(target_col="Age", copy=True, return_df=True).fit(
-#         titanic_train
-#     )
-#     df = imputer.transform(titanic_train)
-#     assert df["Age"].isna().sum() == 0
-#     assert id(df) != id(titanic_train)
-#     assert np.all(df.loc[na_mask, "Age"] == 28.0)
-
-
-# def test_numerical_gb1(titanic_data):
-#     """ gb1 stands for grouping by 1 columns """
-#     titanic_train, _ = titanic_data
-#     na_mask = titanic_train["Age"].isna()
-
-#     imputer = ImputeNumericalByGroup(
-#         target_col="Age", groupby_col=["Pclass"], copy=True, return_df=True
-#     ).fit(titanic_train)
-#     df = imputer.transform(titanic_train)
-#     assert df["Age"].isna().sum() == 0
-#     assert id(df) != id(titanic_train)
-#     assert np.all(
-#         df.loc[na_mask].apply(
-#             lambda row: row["Age"] == imputer.imputation_values[(row["Pclass"],)],
-#             axis=1,
-#         )
-#     )
-
-
-# def test_numerical_gb2(titanic_data):
-#     """ gb2 stands for grouping by 2 columns """
-#     titanic_train, _ = titanic_data
-#     na_mask = titanic_train["Age"].isna()
-
-#     imputer = ImputeNumericalByGroup(
-#         target_col="Age", groupby_col=["Pclass", "Embarked"], copy=True, return_df=True
-#     ).fit(titanic_train)
-#     df = imputer.transform(titanic_train)
-#     assert df["Age"].isna().sum() == 0
-#     assert id(df) != id(titanic_train)
-#     assert np.all(
-#         df.loc[na_mask].apply(
-#             lambda row: row["Age"] == imputer.imputation_values[tuple(row[imputer.groupby_col])],
-#             axis=1,
-#         )
-#     )
-
 def test_numerical(titanic_data):
-    titanic_train, _ = titanic_data  
+    """ Fitting and transforming without needing to consider unseen groups (e.g. in test data) """
+    titanic_train, _ = titanic_data
     na_mask = titanic_train["Age"].isna()
 
-    for gb_col in (None, 'Pclass', ['Pclass', 'Embarked']):
+    for gb_col in (None, "Pclass", ["Pclass", "Embarked"]):
         imputer = ImputeNumericalByGroup(
             target_col="Age", groupby_col=gb_col, copy=True, return_df=True
         ).fit(titanic_train)
@@ -84,7 +34,8 @@ def test_numerical(titanic_data):
         if gb_col:
             assert np.all(
                 df.loc[na_mask].apply(
-                    lambda row: row["Age"] == imputer.imputation_values[tuple(row[imputer.groupby_col])],
+                    lambda row: row["Age"]
+                    == imputer.imputation_values[tuple(row[imputer.groupby_col])],
                     axis=1,
                 )
             )
@@ -98,10 +49,11 @@ def test_numerical(titanic_data):
 
 
 def test_categorical(titanic_data):
-    titanic_train, _ = titanic_data  
+    """ Fitting and transforming without needing to consider unseen groups (e.g. in test data) """
+    titanic_train, _ = titanic_data
     na_mask = titanic_train["Embarked"].isna()
 
-    for gb_col in (None, 'Pclass'):
+    for gb_col in (None, "Pclass"):
         imputer = ImputeCategoricalByGroup(
             target_col="Embarked", groupby_col=gb_col, copy=True, return_df=True
         ).fit(titanic_train)
@@ -113,7 +65,8 @@ def test_categorical(titanic_data):
         if gb_col:
             assert np.all(
                 df.loc[na_mask].apply(
-                    lambda row: row["Embarked"] == imputer.imputation_values[tuple(row[imputer.groupby_col])],
+                    lambda row: row["Embarked"]
+                    == imputer.imputation_values[tuple(row[imputer.groupby_col])],
                     axis=1,
                 )
             )
