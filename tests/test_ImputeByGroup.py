8aa9b05ec2decb244ca5e138f25b5aac1c5102b6
Everest Law <everestl@usc.edu>
2021-04-05 00:33:36 -0700

    Test for case when transforming unseen group

--- tests/test_ImputeByGroup.py
+++ tests/test_ImputeByGroup.py
@@ -17,6 +17,20 @@ def titanic_data():
     return titanic_train, titanic_test
 
 
+@pytest.fixture
+def mismatch_group_numerical_data():
+    df_train = pd.DataFrame({
+        "key_a": ["Adam"]*4 + ["Eve"]*4,
+        "value": [1,2,3,4] + [5,6,7,8],
+    })
+
+    df_test = pd.DataFrame({
+        "key_a": ["Adam"]*4 + ["Peter"]*4,
+        "value": [1,2,np.nan,4] + [np.nan]*4
+    })
+    return df_train, df_test
+
+
 def test_numerical(titanic_data):
     """ Fitting and transforming without needing to consider unseen groups (e.g. in test data) """
     titanic_train, _ = titanic_data
@@ -77,3 +91,39 @@ def test_categorical(titanic_data):
                     axis=1,
                 )
             )
+
+
+def test_mismatch_numerical(mismatch_group_numerical_data):
+    """ 
+        Examine behavior of numerical imputer when encountering unseen groups 
+
+        With groups seen during fitting, impute from dict imputation_values.
+        In contrast, impute rows of unseen groups using dataframe-wide statistics 
+        OR throw a KeyError. Choose between the two behaviors via keyword argument.
+    """
+    train, test = mismatch_group_numerical_data
+
+    # Impute missing groups with df-wide stats
+    imputer = ImputeNumericalByGroup(
+        target_col="value", 
+        groupby_col="key_a",
+        return_df=True,
+        copy=True,
+        key_error_on_unseen=False
+    ).fit(train)
+
+    imputed_test = imputer.transform(test)
+    assert np.all(imputed_test.query("`key_a` == 'Peter'")["value"] == train.value.median())
+    assert imputed_test.iloc[2, 1] == train.query("`key_a` == 'Adam'")["value"].median()
+
+    # Raise KeyError
+    imputer = ImputeNumericalByGroup(
+        target_col="value", 
+        groupby_col="key_a",
+        return_df=True,
+        copy=True,
+        key_error_on_unseen=True
+    ).fit(train)
+
+    with pytest.raises(KeyError):
+        _ = imputer.transform(test)
